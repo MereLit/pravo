18afc53363df04be6450e22161a0ec9d9ce4b38a
Julian <ymerenich1998@gmail.com>
2017-12-21 00:28:00 +0200

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  
+  root to: 'pages#index'
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
