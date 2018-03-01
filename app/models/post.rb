6a8e7f8db94cb0030833fd1aab96b4d683ca09b4
Civil21 <diusolexander@gmail.com>
2018-03-01 10:56:59 +0200

    add image to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -6,4 +6,7 @@ class Post < ApplicationRecord
 	validates :title, uniqueness: true
 	validates :title, presence: true
 
+	has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
+
 end
