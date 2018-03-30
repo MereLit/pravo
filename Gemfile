da6b427c6996aff8b9faf28db8803b3bddb051dd
Peter <peter@Developer.P>
2018-03-30 23:14:42 +0300

    validaton tests

--- Gemfile
+++ Gemfile
@@ -64,6 +64,7 @@ end
 group :development do
   # Access an IRB console on exception pages or by using <%= console %> anywhere in the code.
   gem 'web-console', '>= 3.3.0'
+  gem 'rspec-rails', '~> 3.0.0'
   gem 'listen', '>= 3.0.5', '< 3.2'
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
@@ -75,6 +76,7 @@ group :test do
     gem 'rspec-rails', '~> 3.0.0'
     gem 'factory_girl_rails'
     gem 'capybara', '~> 2.13'
+    
 
 end
 
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+
+	RSpec.describe User, :type => :model do
+		context "validation tests" do
+
+			it "ensures nickname presence" do
+
+				user = User.new(password: "qwerty123", email: "student@gmail.com").save
+				expect(user).to eq(false)
+
+			end
+
+
+
+
+			it "ensures password presence" do
+
+				user = User.new(nickname: "nickname", email: "student@gmail.com").save
+				expect(user).to eq(false)
+
+			end
+
+
+			it "ensures email presence" do
+
+				user = User.new(nickname: "nickname", password: "qwerty123").save
+				expect(user).to eq(false)
+
+			end
+
+
+
+			it "successful save" do
+
+				user = User.new(nickname: "nickname", password: "qwerty123",  email: "student@gmail.com").save
+				expect(user).to eq(true)
+
+			end
+
+
+
+		end
+
+
+
+end
