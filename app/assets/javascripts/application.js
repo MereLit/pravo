2e02a8a17fd27e1be93d348f1bbdd820c4cdaf44
Roma-1999 <romanlitvinchuk@gmail.com>
2018-04-20 12:39:35 +0300

    fix_js_and_views

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,18 +17,40 @@
 //= require turbolinks
 //= require rails.validations
 //= require_tree .
-$(function(){
-	$(".dropdown").hover(            
-            function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeIn("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            },
-            function() {
-                $('.dropdown-menu', this).stop( true, true ).fadeOut("fast");
-                $(this).toggleClass('open');
-                $('b', this).toggleClass("caret caret-up");                
-            }
-    );
-});
 
+
+
+<script type="text/javascript">
+function provGuest() {
+obj_output_div=document.getElementById('output1');
+obj_form=document.forms.Guest;
+obj_pole_name =obj_form.name;
+obj_pole_email =obj_form.email;
+obj_pole_message =obj_form.message;
+*
+if (obj_pole_name.value==""){
+obj_output_div.innerHTML = "Напишите свое имя!"; 
+return;
+}
+txt = obj_pole_email.value;
+if (txt==""){
+obj_output_div.innerHTML = "Напишите Ваш E-mail!";
+return;
+}
+if ((txt.indexOf("@") == -1) || (txt.indexOf(".") == -1)) {
+obj_output_div.innerHTML = "Введите корректный E-mail типа [email]name@mail.ru[/email]"; 
+return; 
+} 
+textMessage=obj_pole_message.value;
+if (textMessage==""){ 
+obj_output_div.innerHTML = "Напишите сообщение!";
+return;
+} 
+if (textMessage.length<10){
+obj_output_div.innerHTML = "Длина сообщения должна быть не менее 10 символов";
+return;
+}
+*
+obj_form.submit();
+}
+</script>
