04d50ce5b40584c2afb1ac476413aa92c7376f33
Julian <ymerenich1998@gmail.com>
2018-05-10 18:48:18 +0300

    add locale in this project^_^

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :users
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|uk/ do
   root to: 'pages#index'
+  resources :posts do
+	resources :postcomments
+  end
+  resources :events
   get "/not_permission", to: "pages#NotPermission", as:"not_permission"
   get 'pages/info'
   get 'pages/abit'
-  get 'pages/stud'
+  get 'pages/stud' 
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :posts do
-  	resources :postcomments
-
-  end
-  resources :events
-
-
+  
+end
 end
  
