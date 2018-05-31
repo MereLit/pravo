7bfec5701d02e0cf965b81aaea2685495416d219
Peter <peter@Developer.P>
2018-05-31 06:29:21 +0300

    upd user tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,18 @@ require 'rails_helper'
 	RSpec.describe User, :type => :model do
 		
 
+		it{should have_many(:posts)}
+		it{should have_many(:postcomments)}
+
+		describe "validations" do
+    		it{ is_expected.to validate_presence_of :nickname }
+    		it{ is_expected.to validate_presence_of :email }
+    		it{ is_expected.to validate_uniqueness_of :nickname }
+    		# it{ is_expected.to validate_uniqueness_of :email }
+    
+  		end
+
+
 		context "validation tests" do
 
 			it "ensures nickname presence" do
@@ -33,13 +45,7 @@ require 'rails_helper'
 
 
 
-			# it "unsuccessful save" do
-
-			# 	user = User.new(nickname: "nickname", password: "qwerty123",  email: "student@gmail.com").save
-			# 	expect(user).to eq(false)
-
-			# end
-
+	
 
 
 		end
