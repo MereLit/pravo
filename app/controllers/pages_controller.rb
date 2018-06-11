ec087c791afe16f4e4a1e786f77495a1183b92ad
Julian <31766151+ymerenich1998@users.noreply.github.com>
2018-06-11 03:29:08 +0300

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,11 +5,7 @@ class PagesController < ApplicationController
   @events = Event.all.order(created_at: :DESC)
   @pets = Pet.all.order(created_at: :DESC)
   end
-<<<<<<< HEAD
-
-=======
- 
->>>>>>> 0e85ce8e7b0a0ba37c4dc502d064aeba91c93b35
+  
   def info
   	@users = User.all
   end
